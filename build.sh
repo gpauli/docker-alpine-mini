b74f2b433da9d521cfca1a95ca610b540de9c97b
Gerd Pauli <gp@high-consulting.de>
2019-04-29 11:43:06 +0200

    alpine version 3.9.3

--- build.sh
+++ build.sh
@@ -12,12 +12,13 @@ function makeit_file() {
 }
 
 
-wget http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/x86/alpine-minirootfs-3.9.2-x86.tar.gz
-makeit_file alpine-minirootfs-3.9.2-x86.tar.gz alpine-mini-i386:3.9.2
-rm alpine-minirootfs-3.9.2-x86.tar.gz
+wget http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/x86/alpine-minirootfs-3.9.3-x86.tar.gz
+makeit_file alpine-minirootfs-3.9.3-x86.tar.gz alpine-mini-i386:3.9.3
+rm alpine-minirootfs-3.9.3-x86.tar.gz
 
-wget http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/x86_64/alpine-minirootfs-3.9.2-x86_64.tar.gz
-makeit_file alpine-minirootfs-3.9.2-x86_64.tar.gz alpine-mini-amd64:3.9.2
-rm alpine-minirootfs-3.9.2-x86_64.tar.gz
+wget http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/x86_64/alpine-minirootfs-3.9.3-x86_64.tar.gz
+makeit_file alpine-minirootfs-3.9.3-x86_64.tar.gz alpine-mini-amd64:3.9.3
+rm alpine-minirootfs-3.9.3-x86_64.tar.gz
+
 
 
